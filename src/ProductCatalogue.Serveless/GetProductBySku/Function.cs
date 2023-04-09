0580401e7a7c5fd19edd430204edd576ce17b818
pmarcelojr <marcelosantostecnologia@gmail.com>
2023-04-09 19:40:58 -0300

    fix bug in sln and implementation two lambda functions

--- src/ProductCatalogue.Serveless/GetProductBySku/Function.cs
+++ src/ProductCatalogue.Serveless/GetProductBySku/Function.cs
@@ -1,4 +1,14 @@
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Ardalis.GuardClauses;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using ProductCatalogue.Application.Queries;
+using ProductCatalogue.Application.Setup;
+using ProductCatalogue.Infrastructure;
+using ProductCatalogue.Infrastructure.LambdaLogger;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,16 +17,70 @@ namespace GetProductBySku
 {
     public class Function
     {
+        private readonly IServiceCollection _serviceCollection;
+        private readonly ServiceProvider _serviceProvider;
+        private readonly Guid _tenantId;
+        private readonly Lazy<IMediator> _mediatr;
 
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        // example payload { "tenantId": "743872ea-7e68-421b-9f98-e09f35d76117", "sku": "HOU/IN/82" }
+        public Function()
         {
-            return input.ToUpper();
+            _serviceCollection = new ServiceCollection()
+                .AddApplicationServices()
+                .AddPersistenceServices();
+            _serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            _mediatr = new Lazy<IMediator>(() => _serviceProvider.GetRequiredService<IMediator>());
+
+            // JUST FOR TESTING, forces the tenant ID to be a known one so the user doesn't have to remember it
+            this._tenantId = Guid.Parse("743872ea-7e68-421b-9f98-e09f35d76117");
+        }
+
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var logger = _serviceProvider.GetRequiredService<ILogger>();
+            logger.SetLoggerContext(context.Logger);
+            logger.LogInfo($"Fetching product by SKU for tenant {_tenantId}");
+
+            try
+            {
+                // just for testing, the tenant id would be a guid in a real app
+                Guard.Against.Null(request, nameof(request));
+                var query = JsonConvert.DeserializeObject<GetProductBySkuQuery>(request.Body);
+
+                // fire command (tenantId should come from the data passed but this is for testing)
+                query.TenantId = _tenantId;
+
+                logger.LogInfo($"Fetching product by SKU {query.Sku} for tenant {query.TenantId}");
+                var queryResult = await _mediatr.Value.Send(new GetProductBySkuQuery(query.TenantId, query.Sku));
+
+                // return result
+                if (queryResult == null)
+                {
+                    logger.LogInfo($"Product with SKU {query.Sku} not found for tenant {query.TenantId}");
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = (int)HttpStatusCode.NotFound,
+                        Body = $"Product with SKU {query.Sku} not found for tenant {query.TenantId}"
+                    };
+                }
+
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = JsonConvert.SerializeObject(queryResult)
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error fetching product by SKU for tenant {_tenantId} - {ex.Message}");
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = $"Error fetching product by SKU for tenant {_tenantId} - {ex.Message}"
+                };
+            }
         }
     }
 }
--- src/ProductCatalogue.Serveless/GetProducts/Function.cs
+++ src/ProductCatalogue.Serveless/GetProducts/Function.cs
@@ -1,4 +1,13 @@
+using System.Net;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using ProductCatalogue.Application.Queries;
+using ProductCatalogue.Application.Setup;
+using ProductCatalogue.Infrastructure;
+using ProductCatalogue.Infrastructure.LambdaLogger;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -8,15 +17,56 @@ namespace GetProducts
     public class Function
     {
 
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        private readonly IServiceCollection _serviceCollection;
+        private readonly ServiceProvider _serviceProvider;
+        private Guid _tenantId;
+        private readonly Lazy<IMediator> _mediatr;
+
+        public Function()
         {
-            return input.ToUpper();
+            _serviceCollection = new ServiceCollection()
+                .AddApplicationServices()
+                .AddPersistenceServices();
+            _serviceProvider = _serviceCollection.BuildServiceProvider();
+
+            _mediatr = new Lazy<IMediator>(() => _serviceProvider.GetRequiredService<IMediator>());
+
+            // JUST FOR TESTING, forces the tenant ID to be a known one so the user doesn't have to remember it
+            _tenantId = Guid.Parse("743872ea-7e68-421b-9f98-e09f35d76117");
+        }
+
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            // performs a query for all products for a tenant (tenant id would be from a parameter in real life).
+            var logger = _serviceProvider.GetRequiredService<ILogger>();
+            logger.SetLoggerContext(context.Logger);
+            logger.LogInfo($"Fetching all products for tenant {_tenantId}");
+
+            try
+            {
+                var query = new GetProductsQuery(_tenantId);
+                var queryResponse = await _mediatr.Value.Send(query);
+
+                logger.LogInfo($"Returning {queryResponse.Count()} products");
+
+                // return result
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = JsonConvert.SerializeObject(queryResponse),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+                };
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error fetching products: {ex.Message}");
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = JsonConvert.SerializeObject(ex.Message),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+                };
+            }
         }
     }
 }
